8e2561d1a6e6da723ecb94ce7c7c5babbea5205c
Brett Hartman <brett.hartman@mac.com>
2019-07-23 16:56:29 -0700

    Add functionality for registering and triggering callbacks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
-user.set({ age: 999 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered');
+});
+
+user.trigger('change');
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,9 +3,11 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => {};
+type Callback = () => void;
 
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -18,6 +20,20 @@ export class User {
   }
 
   on(eventName: string, callback: Callback): void {
-    //
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(callback => {
+      callback();
+    });
   }
 }
